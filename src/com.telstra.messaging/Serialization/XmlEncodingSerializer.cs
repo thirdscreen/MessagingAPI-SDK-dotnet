5c9d7bd878f6a774746eef26a92901ce2f0ab320
Scott Malane <sm@thirdscreen.com.au>
2019-09-11 11:08:23 +1000

    Xml encode message subject.

---
+++ src/com.telstra.messaging/Serialization/XmlEncodingSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace com.telstra.messaging.Serialization
+{
+    internal class XmlEncodingSerializer : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return XmlReader.Create(new StringReader("<r>" + existingValue.ToString() + "</r>")).ReadElementString();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(new XText(value.ToString()).ToString());
+        }
+    }
+}
